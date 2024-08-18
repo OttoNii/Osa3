f463dcac7d15b5ebcdd11387b901d634a713e970
OttoNii <otto.niittykoski@gmail.com>
2024-08-18 10:26:28 +0300

    3.15

--- index.js
+++ index.js
@@ -54,10 +54,11 @@ let persons = [
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
