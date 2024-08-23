e0b434313bcfc84a48cdf6c894d728131e39f8e5
OttoNii <otto.niittykoski@gmail.com>
2024-08-23 12:40:01 +0300

    3.19 backend2

--- index.js
+++ index.js
@@ -73,9 +73,9 @@ app.delete('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (body.content === undefined) {
+  /*if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
-  }
+  }*/
 
   const person = new Person({
     name: body.name,
