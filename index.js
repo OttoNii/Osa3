2bbe04e41eb3bde00f99d86c8893f114fd529339
OttoNii <otto.niittykoski@gmail.com>
2024-08-27 13:28:12 +0300

    3.22

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ const requestLogger = (request, response, next) => {
 }
 
 
-
-let persons = [
-  ]
-
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
@@ -31,26 +27,26 @@ app.use(requestLogger)
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
-  
+
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response, next) => {
   Person.countDocuments({})
-  .then(count => {
-    const message = `Phonebook has info for ${count} people`
-    const timeOfRequest = new Date()
-    response.send(`<h1>${message}</h1><p>${timeOfRequest}</p>`)
-  })
-  .catch(error => next(error))
-  
- })
-  
+    .then(count => {
+      const message = `Phonebook has info for ${count} people`
+      const timeOfRequest = new Date()
+      response.send(`<h1>${message}</h1><p>${timeOfRequest}</p>`)
+    })
+    .catch(error => next(error))
+
+})
+
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
@@ -59,15 +55,15 @@ app.get('/api/persons/:id', (request, response, next) => {
       response.status(404).end()
     }
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
@@ -85,14 +81,14 @@ app.post('/api/persons', (request, response, next) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const {name, number} = request.body
+  const { name, number } = request.body
 
   Person.findByIdAndUpdate(request.params.id,
-    {name, number},
+    { name, number },
     { new: true, runValidators: true, context: 'query' }
   )
     .then(updatedPerson => {
--- models/person.js
+++ models/person.js
@@ -9,33 +9,33 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
 
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name:{
-      type: String,
-      minlength: 3,
-      required: true
-    },
-    number:{
-      type: String,
-      minlength: 9,
-      validate: {
-        validator: function(v) {
-          return /\d{2,3}-\d{5,6}/.test(v);
-        },
+const personSchema = new mongoose.Schema({
+  name:{
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number:{
+    type: String,
+    minlength: 9,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d{5,6}/.test(v)
       },
-      required: true
-    }
-  })
+    },
+    required: true
+  }
+})
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
+  transform: (_document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
